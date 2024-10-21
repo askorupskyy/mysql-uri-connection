43f88d9cb4078f0a33a88bb24a134a09d19f61c6
askorupskyy <50280805+rcbxd@users.noreply.github.com>
2024-10-21 00:10:01 -0500

    init

---
+++ mysql-uri-connection.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+MYSQL_URL=$1
+
+# use regex to parse the MySQL URL
+if [[ $MYSQL_URL =~ mysql://([^:]+)(:([^@]*))?@([^:]+):([0-9]+)/([a-zA-Z0-9_]+) ]]; then
+	USERNAME="${BASH_REMATCH[1]}"
+	PASSWORD="${BASH_REMATCH[3]}" # This will be empty if the password is not provided
+	HOSTNAME="${BASH_REMATCH[4]}"
+	PORT="${BASH_REMATCH[5]}"
+	DATABASE="${BASH_REMATCH[6]}"
+else
+	echo "Invalid MySQL URL format"
+	exit 1
+fi
+
+if [[ -n "$PASSWORD" ]]; then
+	# if password is provided in the URL, pass it with `-p`
+	mysql -h "$HOSTNAME" -P "$PORT" -u "$USERNAME" --password="$PASSWORD" "$DATABASE" "$@"
+else
+	# if no password is provided, prompt for it interactively
+	mysql -h "$HOSTNAME" -P "$PORT" -u "$USERNAME" "$DATABASE" "$@"
+fi
